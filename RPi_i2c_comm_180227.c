3601bb26bbf6830d58d12fe62b6fbb9a374b3d5a
7of9 <yasokada@gmail.com>
2018-03-01 17:24:47 +0900

    RPi_i2c_comm_180227: add i2c_send_slaveAddress()

--- RPi_i2c_comm_180227.c
+++ RPi_i2c_comm_180227.c
@@ -5,6 +5,8 @@
 #include "wait_msec_nsec_180301.h"
 
 /*
+ * v0.7 Mar. 01, 2018
+ *   - add i2c_send_slaveAddress()
  * v0.6 Mar. 01, 2018
  *   - add i2c_setStopCondition()
  *   - add i2c_setStartCondition()
@@ -81,6 +83,44 @@ void i2c_setStopCondition(void)
     myDelay();
 }
 
+void i2c_send_slaveAddress(int address_7bit, bool bfRead)
+{
+    int loop;
+    bool bitVal;
+    char slvAdr;
+
+    slvAdr = address_7bit;
+
+    // just in case
+    gpio_setDirection(GPIO_SDA, /* bfOut=*/true);
+
+	// 1. slave address
+    for(loop=0; loop<7; loop++) { // 7bit
+        bitVal = (slvAdr & 0x40);
+        gpio_setLevel(GPIO_SCL, GPIO_LOW);
+        if (bitVal) {
+            gpio_setLevel(GPIO_SDA, GPIO_HIGH);
+		} else {
+            gpio_setLevel(GPIO_SDA, GPIO_LOW);
+		}
+		myDelay();
+        gpio_setLevel(GPIO_SCL, GPIO_HIGH);
+		myDelay();
+		slvAdr <<= 1;
+	}
+
+	// 2. Read/Write-bit
+    gpio_setLevel(GPIO_SCL, GPIO_LOW);
+	if (bfRead) {
+        gpio_setLevel(GPIO_SDA, GPIO_HIGH);
+	} else {
+        gpio_setLevel(GPIO_SDA, GPIO_LOW);
+	}
+	myDelay();
+    gpio_setLevel(GPIO_SCL, GPIO_HIGH);
+    myDelay();
+}
+
 void test_clockout_ioin(void)
 {
     int loop;
@@ -106,12 +146,13 @@ void test_clockout_ioin(void)
 
 int main()
 {
-    i2c_setup();
+	int slvAdr = 0x44; // Slave address
 
+    i2c_setup();
     i2c_setStartCondition();
+    i2c_send_slaveAddress(slvAdr, /*bfRead=*/false);
 
     i2c_setStopCondition();
-
     i2c_teardown();
 
     return 0;
