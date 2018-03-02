d7b17d94a1138ec876e64b6b3a9136a007709e5e
7of9 <yasokada@gmail.com>
2018-03-02 18:09:04 +0900

    gpio_handle_180301.h: has function declarations

--- gpio_handle_180301.h
+++ gpio_handle_180301.h
@@ -4,4 +4,9 @@
 #define GPIO_HIGH (true)
 #define GPIO_LOW (false)
 
+void gpio_setExport(int index, bool bfOn);
+void gpio_setLevel(int index, bool bfHigh);
+int gpio_getLevel(int index);
+void gpio_setDirection(int index, bool bfOut);
+
 #endif
